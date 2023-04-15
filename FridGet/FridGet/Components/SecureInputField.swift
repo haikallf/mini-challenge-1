d532f60872ee3a87b3d3b655412ac24bd342c9c0
haikallf <haikallzrd@gmail.com>
2023-04-16 00:52:37 +0700

    add reusable secure field with show hide functionality

---
+++ FridGet/FridGet/Components/SecureInputField.swift
@@ -0,0 +1,20 @@
+//
+//  SecureInputField.swift
+//  FridGet
+//
+//  Created by Haikal Lazuardi Fadil on 16/04/23.
+//
+
+import SwiftUI
+
+struct SecureInputField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecureInputField_Previews: PreviewProvider {
+    static var previews: some View {
+        SecureInputField()
+    }
+}
