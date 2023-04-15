ef46d9d494c9c4ad49ad0c5a0358f75c7a36d021
haikallf <haikallzrd@gmail.com>
2023-04-16 00:42:07 +0700

    add pre auth view with working navigation

---
+++ FridGet/FridGet/Views/PreAuthView.swift
@@ -0,0 +1,20 @@
+//
+//  PreAuthView.swift
+//  FridGet
+//
+//  Created by Haikal Lazuardi Fadil on 16/04/23.
+//
+
+import SwiftUI
+
+struct PreAuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PreAuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreAuthView()
+    }
+}
