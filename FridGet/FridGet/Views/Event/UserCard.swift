dddbe15646ad43a1242689ad872e5618142cfaf8
haikallf <haikallzrd@gmail.com>
2023-04-30 02:11:09 +0700

    add event detail view, user card, and more color set

---
+++ FridGet/FridGet/Views/Event/UserCard.swift
@@ -0,0 +1,54 @@
+//
+//  UserCard.swift
+//  FridGet
+//
+//  Created by Haikal Lazuardi on 30/04/23.
+//
+
+import SwiftUI
+
+struct UserCard: View {
+    var status: String // accepted, rejected, pending
+    
+    var body: some View {
+        HStack {
+            Text("HA")
+                .font(.system(size: 12))
+                .padding(.all, 10)
+                .background(.gray)
+                .foregroundColor(.white)
+                .cornerRadius(.infinity)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Haikal")
+                    .font(.system(size: 17))
+                    .fontWeight(.semibold)
+                
+                switch status {
+                case "accepted":
+                    Text("Accept the invitation")
+                        .font(.system(size: 12))
+                        .foregroundColor(Color("green"))
+                    
+                case "rejected":
+                    Text("Reject the invitation")
+                        .font(.system(size: 12))
+                        .foregroundColor(Color("red"))
+                    
+                default:
+                    Text("Pending invitation")
+                        .font(.system(size: 12))
+                        .foregroundColor(Color("yellow"))
+                }
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct UserCard_Previews: PreviewProvider {
+    static var previews: some View {
+        UserCard(status: "pending")
+    }
+}
