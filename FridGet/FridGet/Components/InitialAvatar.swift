f8e5a55e54b9b68c138e022c7e2ca47655222a4e
haikallf <haikallzrd@gmail.com>
2023-05-05 16:22:56 +0700

    fix initial avatar auto resize when only display 1 letter initial

--- FridGet/FridGet/Components/InitialAvatar.swift
+++ FridGet/FridGet/Components/InitialAvatar.swift
@@ -10,17 +10,20 @@ import SwiftUI
 struct InitialAvatar: View {
     var initial: String
     var fontSize: CGFloat = 12
+    var size: CGFloat = 32
     
     var body: some View {
-        Text(initial)
-            .font(.system(size: fontSize))
-            .padding(.all, 10)
-            .background(
-                LinearGradient(gradient: Gradient(colors: [Color(red: 188/255, green: 188/255, blue: 188/255),
-                                                           Color(red: 134/255, green: 126/255, blue: 128/255)]), startPoint: .top, endPoint: .bottom)
-            )
-            .foregroundColor(.white)
-            .cornerRadius(.infinity)
+        ZStack {
+            Circle()
+                .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 188/255, green: 188/255, blue: 188/255),
+                                                                             Color(red: 134/255, green: 126/255, blue: 128/255)]), startPoint: .top, endPoint: .bottom))
+                .frame(width: size)
+            Text(initial)
+                .foregroundColor(.white)
+                .font(.system(size: fontSize))
+
+        }
+            
     }
 }
 
